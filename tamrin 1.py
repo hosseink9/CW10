c4f758b002f92b86f2f19dedd96b72f8bc2c9939
hosseink9 <hosseinkalantari.29@gmail.com>
2023-06-08 15:43:31 +0330

    Fixed code

--- tamrin 1.py
+++ tamrin 1.py
@@ -1,5 +1,6 @@
 class Fib:
-    def __init__(self) -> None:
+    def __init__(self,n) -> None:
+        self.n=n
         self.one=0
         self.two=1
 
@@ -8,17 +9,22 @@ class Fib:
         return self
     
     def __next__(self):
+        if self.n ==0:
+            raise StopIteration
+        self.n+=-1
         a=self.one
         nxt=self.one + self.two
         self.one=self.two
         self.two=nxt
         return a
 
-fibo=Fib()
+fibo=Fib(5)
 
 print(next(fibo))#0
 print(next(fibo))#1
 print(next(fibo))#1
 print(next(fibo))#2
 print(next(fibo))#3
+print(next(fibo))
+
 
