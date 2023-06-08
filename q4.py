518128216231f56155c4155d0b74c092865fe59c
Farzam <ffarzam_1992@yahoo.com>
2023-06-08 16:44:19 +0330

    adding  4q.py

---
+++ q4.py
@@ -0,0 +1,28 @@
+
+counter = 0
+
+def inc():
+
+    global counter
+    counter+=1
+    return counter
+
+print(inc())
+
+def outer():
+    var=0
+    def inner():
+        nonlocal var
+        var+=1
+    inner()
+    return var
+
+print(outer())
+
+
+glob=0
+def func():
+    glob=4
+    print(glob)
+
+func()
