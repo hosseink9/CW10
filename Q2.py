65b5cf732b2afc809565114232147c838a3a10b8
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-06-08 16:16:39 +0330

    Fibo iterator

--- Q2.py
+++ Q2.py
@@ -1,5 +1,28 @@
 
 
-class Fibonacciterator:
+class FibonacciIterator:
 
-    def
+    def __init__(self, n):
+        self.end = n
+        self.first = 0
+        self.second = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.end == 0:
+            raise StopIteration
+        self.end -= 1
+        res = self.first
+        next_fibo = self.first + self.second
+        self.first = self.second
+        self.second = next_fibo
+        return res
+
+
+for i in FibonacciIterator(10):
+    print(i)
+
+
+
